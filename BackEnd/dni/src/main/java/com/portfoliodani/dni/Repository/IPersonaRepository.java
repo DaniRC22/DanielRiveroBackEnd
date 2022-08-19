d9e1a0f62ae7e21631d34915f0510b640ec43923
DaniCode <Edanielrivero@hotmail.com>
2022-08-19 14:11:52 -0300

    BackEnd

---
+++ BackEnd/dni/src/main/java/com/portfoliodani/dni/Repository/IPersonaRepository.java
@@ -0,0 +1,10 @@
+package com.portfoliodani.dni.Repository;
+
+import com.portfoliodani.dni.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona, Long> {
+    
+}
---
+++ BackEnd/dni/src/test/java/com/portfoliodani/dni/DniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfoliodani.dni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
