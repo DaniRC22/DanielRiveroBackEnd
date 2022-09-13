b1c96581ba5d389cc18e5d034e4d9976b1acc7f9
DaniCode <Edanielrivero@hotmail.com>
2022-09-13 16:50:53 -0300

    detalles

--- BackEnd/dni/src/main/java/com/portfoliodani/dni/Security/Controller/AuthController.java
+++ BackEnd/dni/src/main/java/com/portfoliodani/dni/Security/Controller/AuthController.java
@@ -34,7 +34,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "https://frontenddani.web.app/")
+@CrossOrigin()
 public class AuthController {
 
     @Autowired
